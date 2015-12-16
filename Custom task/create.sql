ee3dd6d6b8fdc63f40977cf269133fc5b6597eee
Kocsis Viktor <achilleus88888@gmail.com>
2015-12-16 11:59:17 +0100

    repair

--- Custom task/create.sql
+++ Custom task/create.sql
@@ -1,9 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS leaguedb;
+USE leaguedb;
+
 CREATE TABLE Users(id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
 Nickname VARCHAR(15) NOT NULL,
 Position ENUM('top', 'mid', 'jungle', 'support', 'adc') NOT NULL,
 Region ENUM('EUW', 'EUNE', 'NA', 'KR') NOT NULL,
 League ENUM('Unranked', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Challenger')
-)
+);
 
 CREATE TABLE RankedMatchesStats(id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
 UserId INTEGER NOT NULL,
@@ -12,11 +15,11 @@ Total INTEGER,
 Wins INTEGER,
 Losses INTEGER,
 Win_percent FLOAT
-)
+);
 
 CREATE TABLE Champions(id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
 Name VARCHAR(25) NOT NULL
-)
+);
 
 CREATE TABLE RankedChampionStats(id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
 UserId INTEGER NOT NULL,
@@ -31,4 +34,4 @@ Killspergame FLOAT,
 Deathspergame FLOAT,
 Assistspergame FLOAT,
 KDA FLOAT
-)
+);
--- Custom task/query.sql
+++ Custom task/query.sql
@@ -1,4 +1,8 @@
 SELECT * FROM Users ORDER BY Nickname;
 SELECT DISTINCT Name FROM Champions;
 SELECT UserId, Total, Wins, Losses FROM RankedMatchesStats ORDER BY Win_percent DESC;
-SELECT * FROM RankedChampionsStats WHERE ChampionsId = 13;
+SELECT * FROM RankedChampionStats WHERE ChampionsId = 13;
+SELECT Users.Nickname, Champions.Name, RankedChampionStats.Killspergame, RankedChampionStats.Deathspergame, RankedChampionStats.Assistspergame, rankedchampionstats.Wins, rankedchampionstats.Losses
+FROM Users JOIN RankedChampionStats ON Users.id = RankedChampionStats.UserId
+JOIN Champions ON RankedChampionStats.ChampionsId = Champions.id
+WHERE champions.Name = "Corki";
